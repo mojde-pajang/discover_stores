c58d88a9133bbf987bef0395a73bb0af7503d288
Mojdeh Pajang <mojde.pajang@gmail.com>
2024-12-09 07:39:50 +0100

    Add stores section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
-import Link from "next/link";
 import Banner from "./components/Banner.client";
+import { BOOK_STORES } from "./constants";
+import Card from "./components/Card.server";
 
 export default function Home() {
   const id = "Joseph";
@@ -7,9 +8,17 @@ export default function Home() {
     <div className="grid grid-rows-[10px_1fr_20px] justify-items-center min-h-screen p-3 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start w-full">
         <Banner />
-        <h2 className=" text-bold text-xl">Stores</h2>
-        <div className=" flex gap-3 justify-between flex-shrink ">
-          <Link href={`/stores/${id}`}>Joseph</Link>
+
+        <h2 className=" text-3xl mb-3 font-bold mt-10">Paris Stores</h2>
+        <div className=" grid justify-between gap-6 grid-cols-3 w-full">
+          {BOOK_STORES.map((store, idx) => (
+            <Card
+              key={idx}
+              name={store.name}
+              href={store.href}
+              imageUrl={store.image}
+            />
+          ))}
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
