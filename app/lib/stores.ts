7b7bbe5377c41e507d36b0035d6668ae4bd2fde9
Mojdeh Pajang <mojde.pajang@gmail.com>
2024-12-10 07:06:48 +0100

    shop detail

--- app/lib/stores.ts
+++ app/lib/stores.ts
@@ -2,7 +2,7 @@ import { Feature, Store } from "../types";
 
 const transformCoffeeStore = (shop: Feature): Store => {
   return {
-    id: shop.id,
+    id: shop.id || shop?.properties?.mapbox_id,
     name: shop.properties.name,
     address: shop.properties.full_address,
     imageUrl:
@@ -10,7 +10,7 @@ const transformCoffeeStore = (shop: Feature): Store => {
   };
 };
 
-export const getStores = async () => {
+export const getStores = async (): Promise<[] | Store[]> => {
   try {
     const data = await fetch(
       `https://api.mapbox.com/search/geocode/v6/forward?q=coffee&country=us&limit=6&proximity=-74.00763403803957%2C40.71322172252451&types=place&access_token=${process.env.MAPBOX_TOKEN}`
@@ -19,8 +19,25 @@ export const getStores = async () => {
     const coffeeStores: Store[] = features.map((feature: Feature) =>
       transformCoffeeStore(feature)
     );
-    return coffeeStores;
+    return coffeeStores.length || undefined ? coffeeStores : [];
   } catch (error) {
     console.error("Error fetching", error);
+    return [];
+  }
+};
+
+export const getStore = async (id: string): Promise<Store | null> => {
+  try {
+    const data = await fetch(
+      `https://api.mapbox.com/search/searchbox/v1/retrieve/${id}?session_token=0697ab44-3236-4491-893a-bc5803c05869&access_token=${process.env.MAPBOX_TOKEN}`
+    );
+    const { features } = await data.json();
+    const coffeeStore: Store[] = features?.map((feature: Feature) =>
+      transformCoffeeStore(feature)
+    );
+    return coffeeStore.length > 0 ? coffeeStore[0] : null;
+  } catch (error) {
+    console.error("Error fetching", error);
+    return null;
   }
 };
--- app/stores/[id]/page.tsx
+++ app/stores/[id]/page.tsx
@@ -1,10 +1,57 @@
+import { getStore, getStores } from "@/app/lib/stores";
+import { Store } from "@/app/types";
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-function Page() {
+export async function generateStaticParams() {
+  const stores = await getStores();
+
+  return stores.map((store: Store) => ({
+    id: store.id,
+  }));
+}
+async function Page({ params }: any) {
+  const { id } = await params;
+  const store = await getStore(id);
+  if (!store) {
+    return (
+      <div className="h-full pb-80">
+        <div className="m-auto grid max-w-full px-12 py-12 lg:max-w-6xl lg:grid-cols-2 lg:gap-4">
+          <div className="mb-2 mt-24 text-lg font-bold">
+            <Link href="/">← Back to home</Link>
+          </div>
+          <div>Store not found</div>;
+        </div>
+      </div>
+    );
+  }
   return (
-    <div>
-      <Link href="/">← Back to home</Link>
+    <div className="h-full pb-80">
+      <div className="m-auto grid max-w-full px-12 py-12 lg:max-w-6xl lg:grid-cols-2 lg:gap-4">
+        <div className="">
+          <div className="mb-2 mt-24 text-lg font-bold">
+            <Link href="/">← Back to home</Link>
+          </div>
+          <div className="my-4">
+            <h1 className="text-4xl">{store?.name}</h1>
+          </div>
+          <Image
+            src={
+              store.imageUrl ||
+              "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
+            }
+            width={740}
+            height={360}
+            className="max-h-[420px] min-w-full max-w-full rounded-lg border-2 sepia lg:max-w-[470px] "
+            alt={"Coffee Store Image"}
+          />
+        </div>
+
+        <div className={`glass mt-12 flex-col rounded-lg p-4 lg:mt-48`}>
+          <h2 className=" text-xl font-bold">{store.address}</h2>
+        </div>
+      </div>
     </div>
   );
 }
